2522328426faa0b0dc08071c847e0305b0b57f44
stefy2906 <stefanybreve@gmail.com>
2023-08-22 11:46:42 -0400

    commit

--- src/middleware.js
+++ src/middleware.js
@@ -6,11 +6,17 @@ export const middleware = (request) => {
 
     const token = request.cookies.get('token')?.value;
     const urlLogin = new URL('/', request.url);
+    const urlDash = new URL("/pages/dashboard",request.url);
     const isTokenValidated = validateToken(token);
 
     if (!isTokenValidated || !token) {
         if (request.nextUrl.pathname === '/pages/dashboard') {
             return NextResponse.redirect(urlLogin);
+        }
+    }
+    if (!isTokenValidated || !token) {
+        if (request.nextUrl.pathname === '/') {
+            return NextResponse.redirect(urlDash);
         }
     }
     NextResponse.next();
