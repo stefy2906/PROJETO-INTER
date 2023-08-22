98842c9ec0ee78a7b3ad173d492c77ee129ee61e
stefy2906 <stefanybreve@gmail.com>
2023-08-22 17:31:51 -0400

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ export default function Login() {
     email: '',
     password: '',
   });
-  const { push, refresh } = useRouter();
+  const { push } = useRouter();
 
   const handlerLogin = async (e) => {
     e.preventDefault();
