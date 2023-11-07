18e9f9f416c2817030a08168a5c7dd1b0a844a9b
mariafernandabarbie <stefanybreve@gmail.com>
2023-11-07 17:23:32 -0400

    commit

--- src/app/functions/validateToken.js
+++ src/app/functions/validateToken.js
@@ -2,11 +2,18 @@ import { jwVerify } from "jose";
 
 const validateToken = async(token) => {
     const secret = "fndkjvnkjdfnvkjnfdjbjniejfieng"
-    
-    if(isTokenValidate){
-        return token= true
+    try {
+        /*const isTokenValidate = await jwtVerify(token, new TextEn)
+        if(ifTokenValidate){
+            return true
+        }*/
+        const isTokenValidate = await decode(token);
+        if(isTokenValidate){
+            return true
+        }
+    } catch (error) {
+        return false;
     }
-    return token=false;
 }   
 
-export {validateToken};
+export { validateToken };
